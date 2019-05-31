4b9b138355b6588a4a1e42a34a4fa716a384e535
xiongyejun <244114746@qq.com>
2019-05-31 18:28:19 +0800

    init

---
+++ copy.cmd
@@ -0,0 +1,9 @@
+copy %GOBIN%\ls.exe ls.exe
+copy %GOBIN%\ribbon.exe ribbon.exe
+copy %GOBIN%\vba.exe vba.exe
+copy %GOBIN%\vbaUI.exe vbaUI.exe
+copy %GOBIN%\lan.exe lan.exe
+copy %GOBIN%\fisql.exe fisql.exe
+copy %GOBIN%\filebyte.exe filebyte.exe
+
+pause
